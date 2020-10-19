a6f8bb4054ecc65493e8d7fa4151c5733b5f2ee8
rmeacham9224 <rcm0025@auburn.edu>
2020-10-18 22:38:24 -0500

    mock variables added for different responses

--- assets/script.js
+++ assets/script.js
@@ -3,19 +3,6 @@ window.alert("Welcome to the random password generator. Your password must inclu
 window.alert("Adjust the length of the password with the sliding bar before generating your password");
 
 function generate() {
-
-    var promptLower = window.prompt('Would you like to add Lowercase letters? Enter "Y" for yes or "N" for no');
-
-    var promptUpper = window.prompt('Would you like to add Uppercase letters? Enter "Y" for yes or "N" for no.');
-
-    var promptNumeric = window.prompt('Would you like to add Numerics? Enter "Y" for yes or "N" for no.');
-
-    var promptSpecial = window.prompt('Would you like to add special characters? Enter "Y" for yes or "N" for no.');
-
-
-
-
-
     //set password length/complexity
     let complexity = document.getElementById("slider").value;
     
@@ -48,131 +35,62 @@ document.getElementById("slider").oninput = function() {
 
 
 
+//var possible = {
+  //  none: [],
+    
+    //confirmlower = window.prompt('Would you like to add Lowercase letters? Enter "Y" for yes or "N" for no'),
+    //lowertrue = function(){
+      //  if(confirmlower === "Y" || "y") {
+        //var low = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
+        //} else {
+        //low = none
+        //}
+    //},
 
+    //up = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"],
+    //confirmupper = window.prompt('Would you like to add Uppercase letters? Enter "Y" for yes or "N" for no'),
+    //uppertrue = function(){
+      //  if (confirmupper === "Y" || "y") {
+        //    this.lowertrue + up
+        //} 
+        //else {
+          //  this.lowertrue + none
+        //}
+    //}, 
 
-//var passwordLength = function() {
-   // var promptLength = window.prompt('How long would you like your password to be? need to adjust this')
-    //need to figure out how to link to what answer they select. could do short medium or long and give ranges.
-    //if (promptLength === "" || promptLength === null){
-      //  window.alert('please enter a value.');
-       // return passwordLength();
-    //}
-//}
-
-//var lowercase = function() {
-  //  
-//
-  //  //nothing entered
-    //if (promptLower === "" || promptLower === null) {
-      //  window.alert('Please enter "Y" or "N"');
-        //return lowercase();
-//    }
-//
-//    promptLower = promptLower.toLowerCase();
-
-    //if person picks no
-  //  if (promptLower === "n") {
-    //    var confirmNoLower = window.confirm("Are you sure? Adding lowercase letters will strengthen your password.");
-      //      if(confirmNoLower) {
-        //        window.alert("Your password will not include lowercase letters.");
-          //      return true;
-            //} else {
-              //  return false;
+    //num = ["1","2","3","4","5","6","7","8","9"],
+    //confirmnum = window.prompt('Would you like to add numeric values? Enter "Y" for yes or "N" for no'),
+    //numtrue = function(){
+      //  if (confirmnum === "Y" || "y") {
+        //    this.uppertrue += num  
+        //} 
+        //else {
+          //  this.uppertrue += none
+        //} 
+    //},
+    //spe: ["!","@","~","%","$","(",")","?", "&"],
+    //confirmspe = window.prompt('Would you like to add special characters? Enter "Y" for yes or "N" for no'),
+    //spetrue = function(){
+      //  if (confirmspe === "Y" || "y") {
+        //    this.numtrue += spe
+        //}
+        //else {
+          //  this.num += none
         //}
     //}
 //}
+        
 
-//var uppercase = function() {
-  //  
 
-    //if (promptUpper === "" || promptUpper === null) {
-      //  window.alert ('Please enter "Y" or "N"');
-        //return uppercase();
-    //}
-
-    //promptUpper = promptUpper.toLowerCase();
-
-    //if  person picks no
-    //if (promptUpper === "n") {
-      //  var confirmNoUpper = window.confirm("Are you sure? Adding uppercase letters will strengthen your password.");
-        //    if (confirmNoUpper) {
-          //      window.alert("Your password will not include uppercase letters.");
-            //    return true;
-            //} else {
-              //  return false;
-       // }
-   // }
-//}
-
-//still need to add numeric function
-//var numeric = function() {
-  //  
-
-    //if (promptNumeric === "" || promptNumeric === null) {
-      //  window.alert ('Please enter "Y" or "N"');
-        //return numeric();
-    //}
-
-    //promptNumeric = promptNumeric.toLowerCase();
-
-    //if person picks no
-   // if (promptNumeric === 'n') {
-     //   var confirmNoNumeric = window.confirm("Are you sure? Adding numerics will strengthen your password.");
-       // if (confirmNoNumeric) {
-         //   window.alert("Your password will not include numerics.");
-           // return true;
-        //} else {
-          //  return false;
-        //}
-    //}
-//}
-
-//var Special = function() {
-  //  
-
-    //if (promptSpecial === "" || promptSpecial === null) {
-      //  window.alert ('Please enter "Y" or "N"');
-        //return Special();
-    //}
-
-    //promptSpecial = promptSpecial.toLowerCase();
-
-    //if person picks no
-    //if (promptSpecial === 'n') {
-      //  var confirmSpecial = window.confirm("Are you sure? Adding special characters will strengthen your password.");
-        //if (confirmSpecial) {
-          //  window.alert("Your password will not include special characters.");
-            //return true;
-        //} else {
-          //  return false;
-       // }
-   // }
-//}
+//var lowup = low.concat(up);
+//var lowspe = low.concat(spe);
+//var upnum = up.concat(num);
+//var upspe = up.concat(spe);
+//var lowupnum = low.concat(up,num);
+//var lowupspe = low.concat(up,spe);
 
 
 
 
-
-//function generate(){
-//}
-
-//generate();
-
-
-
-// Get references to the #generate element
-//var generateBtn = document.querySelector("#generate");
-
-// Write password to the #password input
-//function writePassword() {
-  //var password = generatePassword();
- // var passwordText = document.querySelector("#password");
-
-  //passwordText.value = password;
-
-//}
-
-// Add event listener to generate button
-//generateBtn.addEventListener("click", writePassword);
 
 //generate();
